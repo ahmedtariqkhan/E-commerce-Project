bab968a2e8813503a728ccf0ca09c899eb77c4bc
ahmedtariqkhan <ahmedkhansaduzai@gmail.com>
2024-08-15 21:43:03 +0500

    Initial project changes

---
+++ front-end-admin/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+import { ProgressBarModule } from 'primeng/progressbar';
+
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    ProgressBarModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent{
+}
